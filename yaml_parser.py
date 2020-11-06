36e5ca671a42504e89f31b71be720c558619a95b
Shazibul Islam Shamim <shamim.buet10@gmail.com>
2020-11-06 01:02:03 -0600

    Returning key or values recursively

--- yaml_parser.py
+++ yaml_parser.py
@@ -4,10 +4,45 @@ Parse and Identify the violation of RBAC in the repository
 
 import yaml
 import os
+import constant
 
 source = "/Users/shamim/Downloads/K8s_inspection/GITLAB_REPOS/justin@kubernetes/"
 
-def parse_yaml_file (source_path):
+
+def get_key_values(dictionary_or_list,key_or_value):
+    if(type(dictionary_or_list) is dict):
+        for key,values in dictionary_or_list.items():
+            if((type(values) is dict) or (type(values) is list)):
+                if(key_or_value is return_key):
+                    yield key
+                for value in get_key_values(values,key_or_value):
+                    yield value
+            else:
+                if (key_or_value is return_key):
+                    yield key
+                if (key_or_value is return_value):
+                    yield values
+
+    elif(type(dictionary_or_list) is list):
+        for item in dictionary_or_list:
+            if((type(item) is list) or (type(item) is dict)):
+                for value in get_key_values(item,key_or_value):
+                    yield value
+
+
+#source = "/Users/shamim/Downloads/K8s_inspection/GITLAB_REPOS/"
+#subdirs = os.listdir(source)
+#print(subdirs)
+
+# def find_key(dictionary,keysearch="key"):
+#     for key,value in dictionary.items():
+#         if keysearch = "key":
+#             return key
+
+
+
+def parse_yaml_file(source):
+    count = 0
     for (root, directory, files) in os.walk(source,topdown=True):
         for name in files:
             if name.endswith((".yaml", ".yml")) and not name.endswith(("docker-compose.yml","bootstrap.yml","application,yml")):
@@ -16,20 +51,45 @@ def parse_yaml_file (source_path):
                 #print(file_path)
                 with open(file_path,"r") as file:
                     try:
-                        yaml_file = list(yaml.safe_load_all(file))
-#                       if(yaml_file['kind']=="RoleBinding"):
-                        #print(yaml_file['kind'])
-                        dictionary = {}
-                        dictionary = yaml_file[0]
-                        #print(dictionary)
-                        name_space = dictionary.get('namespace')
-                        if (name_space == "default"):
-                            print("\nInstance found\n")
-                            #print(dictionary)
+                        generator = yaml.safe_load_all(file)
+                        yaml_file = {}
+                        yaml_file = generator.__next__()
+                        print(yaml_file)
+                        print("------"*10)
+                        print("\n"*3)
+                        v = get_key_values(yaml_file, constant.return_value)
+                        for value in v:
+                            print(value)
+                        print("\n" * 3)
+                        print("------"*10)
+
                     except yaml.YAMLError as exc:
                         print(exc)
 
 
+    #return yaml_file
+
+
+def parse(source):
+    count = 0
+    for (root, directory, files) in os.walk(source, topdown=True):
+        for name in files:
+            if name.endswith((".yaml", ".yml")) and not name.endswith(
+                    ("docker-compose.yml", "bootstrap.yml", "application,yml")):
+                # print(name)
+                file_path = root + "/" + name
+                # print(file_path)
+                with open(file_path, "r") as file:
+                    try:
+                        generator = yaml.safe_load_all(file)
+                        yaml_file = {}
+                        yaml_file = generator.__next__()
+
+                    except yaml.YAMLError as exc:
+                        print(exc)
+
+    return yaml_file
 
 if __name__ == "__main__":
     parse_yaml_file(source)
+        #print("Instance Found {}",count)
